ee4f7a70d3d17f85630cf1318b092c99636629d8
Joe <josephbrianchambers@gmail.com>
2013-12-05 10:44:28 -0800

    initial commit. set up mongodb with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocationTweets::Application.config.secret_token = '126f98341b82093ef6da6da6025f69156b9343a55d554ab6a108def6c43e8b8575651b06f4a9094076fd0a4b73b3eb8b547df480419742caea46114b6c6c6f16'
