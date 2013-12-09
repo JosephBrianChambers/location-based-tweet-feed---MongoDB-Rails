0ca91397da14c09e7b81d5387bc41013aaa568dc
Joe <josephbrianchambers@gmail.com>
2013-12-09 11:50:06 -0800

    get clean directory for merge of unlogged commits

---
+++ app/assets/javascripts/tweets-js.js
@@ -0,0 +1,15 @@
+
+  // var myApp = root.myApp = (root.myApp || {});
+  
+  $(document).ready(function () {
+    $('.tweet-query').on('submit', function (event) {
+      //make ajax callto tweets-index and populate content
+      event.preventDefault();
+       
+      debugger
+    });
+  })
+
+  
+  
+  
